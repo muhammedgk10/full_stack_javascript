ce33c4bbf5600e039759a3c1ff496b25ac1864f2
Muhammed Gök <gokmuhammed13@gmail.com>
2025-07-06 08:50:04 +0300

    ders3 bitti

---
+++ javascript/js/ders2.js
@@ -0,0 +1,10 @@
+document.getElementsByClassName("harfler")[0].style.fontSize = "42px";
+document.getElementsByClassName("harfler")[1].style.fontSize = "42px";
+document.getElementsByClassName("harfler")[0].style.color = "white";
+document.getElementsByClassName("harfler")[1].style.color = "white";
+
+document.getElementsByName("kullaniciAdi")[0].style.backgroundColor = "yellow";
+
+
+document.getElementsByTagName("span") [0].style.borderBottomStyle="solid";
+document.getElementsByTagName("span") [2].style.borderBottomSize="2px";
