2b8d403599264115d3d6f2e39444083c9810d26e
Muhammed Gök <gokmuhammed13@gmail.com>
2025-07-06 08:17:40 +0300

    ders2 bitti

--- javascript/js/ders1.js
+++ javascript/js/ders1.js
@@ -1,3 +1,9 @@
 alert("Harici sayfa dosyasından gelen mesaj");
 console.log("Harici sayfa dosyasından gelen mesaj");
 document.write("Harici sayfa dosyasından gelen mesaj");
+
+
+document.getElementById("Baslik").style.fontSize = "52px";
+document.getElementById("icerik").innerHTML= "<b>Bu yazı kalın bir yazıdır.<b>";
+document.getElementById("paragraf").innerText= "Paragrafın yeni cümleler eklenebilir.";
+document.getElementById("AdSoyad").value= "Muhammed Gök";
