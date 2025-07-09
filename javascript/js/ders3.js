ba39abe7fd8a0bf2ae0d9c6714d0dcbfbd972a71
Muhammed Gök <gokmuhammed13@gmail.com>
2025-07-09 07:44:20 +0300

    Ders 4 Bitti

---
+++ javascript/js/ders3.js
@@ -0,0 +1,13 @@
+var sayi=52;
+var oran=1.618;
+var sehir="Şanlıurfa";
+var tercih=true;
+var personel={
+    ad: "Muhammed",
+    soyad: "Gök",
+    yas: 19,
+    meslek: "Mühendis",
+    evlimi: false
+};
+
+
