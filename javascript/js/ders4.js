5e4930a999a05fbc8f36edeec8a8797736dbb14c
Muhammed Gök <gokmuhammed13@gmail.com>
2025-07-10 19:00:11 +0300

    ders5 bitti

---
+++ javascript/js/ders4.js
@@ -0,0 +1,9 @@
+function darkMode(){
+    document.getElementsByTagName("body")[0].style.color = "wihite";
+    document.getElementsByTagName("body")[0].style.backgroundColor = "black";
+}
+
+function lightMode(){
+    document.getElementsByTagName("body")[0].style.color = "black";
+    document.getElementsByTagName("body")[0].style.backgroundColor = "white";
+}
